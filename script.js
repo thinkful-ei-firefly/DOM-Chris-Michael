8ec205484e1609608fd9737b6b088a57de852394
Chris <50714992+cmpowers1981@users.noreply.github.com>
2019-07-11 16:13:06 -0400

    Cat-Carousel - Mike and Chris
    WIll discuss with Mentor

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-'use strict';
-
 $('.thumbnail').on('click', e => {
-  console.log(e.target);
-});
+    console.log(e.target);
+  });
+  $('.thumbnail').on('click', e => {
+    console.log(e.target);
+  });
+
+ function handleThumbnailClicks() {
+
+    $('.thumbnail').on('click', {
+        funtion(event) 
+       
+        const imgSrc = $(this).find('img').attr('src'),
+       const imgAlt = $(this).find('img').attr('alt'),
+        };
+        $('.hero img').attr('src', imgSrc).attr('alt', imgAlt,),
+
+        };
+    }
+
+}
+
+$(handleThumbnailClicks);
